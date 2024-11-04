3d476372f284ccd21e48c524db21cd4f3a05aa84
hmgny <hmerveguney@hotmail.com>
2024-11-05 01:42:01 +0300

    buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,130 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Form, FormGroup, Label, Input, ButtonGroup, Button } from 'reactstrap'
 import './App.css'
 
+const malzemeler = ["Pepperoni","Sosis", "Kanada Jambonu","Tavuk Izgara","Soğan","Domates", "Mısır", "Sucuk", "Jalepeno", "Sarımsak", "Biber", "Sucuk", "Ananas", "Kabak"];
+const boyutlar=["küçük", "orta", "büyük"];
+const hamurSeç = ["ince","orta","kalın"]
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
+
+
+
+
+
+
 
   return (
     <>
+      {/* kırmızı alan............................................ */}
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+        <h1>Teknolojik Yemekler</h1>
+        </div>
+        
+        <div>
+          <button>Anasayfa</button>
+          <button>Sipariş Oluştur</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {/* Position Pizza-Paragraf bölümü........................................... */}
+
+      <div>
+        <h2>Position Absolute Acı Pizza</h2>
+        <div>
+          <p>85.50₺</p>
+          <p>4.9</p>
+          <p>(200)</p>
+        </div>
+        <p>Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta denir</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+
+      {/* Boyut-Hamur bölümü........................................... */}
+
+      <div>
+        <div>
+          <Form>
+            <h2>Boyut Seç <span style={{color: "red"}}>*</span></h2>
+
+            {boyutlar.map((boyut)=>{
+              return <FormGroup>
+              <Input id={boyut} name="boyut" type="radio"/>{" "}
+              <Label htmlFor={boyut}>{boyut}</Label>
+            </FormGroup>
+            })}
+  
+          </Form>
+        </div>
+
+        <div>
+          <h2>Hamur Seç<span style={{color: "red"}}>*</span></h2>
+          <select >
+            <option value="Hamur Kalınlığı" placeholder="Hamur Kalınlığı">İnce</option>
+            <option value="Hamur Kalınlığı" placeholder="Hamur Kalınlığı">Orta</option>
+            <option value="Hamur Kalınlığı" placeholder="Hamur Kalınlığı">Kalın</option>
+          </select>
+        </div>
+      </div>
+      
+      {/* Ek Malzemeler................................................... */}
+
+      <div>
+        <Form>
+          <h2>Ek Malzemeler</h2>
+          <p>En Fazla 10 malzeme seçebilirsiniz. 5₺ <span style={{color: "red"}}>*</span></p>
+
+          { malzemeler.map((malz,index)=>{
+            return <FormGroup key={index}>
+            <Input id={malz} name="malzeme" type="checkbox"/>{" "}
+            <Label htmlFor={malz}>{malz}</Label>
+          </FormGroup>
+          })}
+          
+        </Form>
+      </div>
+
+      {/* Sipariş Notu........................................... */}
+      <div>
+        <div>
+        <FormGroup>
+          <Label>Sipariş Notu</Label>
+          <Input type="textarea" placeholder='Siparişinize eklemek istediğiniz bir not var mı?'/>
+        </FormGroup>
+        </div>
+        
+        <hr></hr>  {/* ÇİZGİ........................................... */}
+
+        <div>
+          <ButtonGroup>
+            <Button>-</Button>
+            <Button>1</Button>
+            <Button>+</Button>
+          </ButtonGroup>
+
+          <div>
+            <h2>Sipariş Toplamı</h2>
+            <div>
+              <p>Seçimler</p>
+              <p>25</p>
+            </div>
+
+            <div>
+              <p>Toplam</p>
+              <p>110</p>
+            </div>
+          </div>
+
+          <Button>SİPARİŞ VER</Button>
+
+        </div>
+      </div>
+     
     </>
   )
 }
