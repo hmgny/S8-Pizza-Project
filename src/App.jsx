65a1f106b1216a4d0e2e8d5eabf0dfc7ab53ac4f
hmgny <hmerveguney@hotmail.com>
2025-02-20 02:42:06 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,26 @@
-import OrderForm from "./components/OrderForm"
-import Home from "./components/Home"
-import Success from "./components/Success"
-import OrderFormT2 from "./components/OrderFormT2"
-import HomeT2 from "./components/HomeT2"
-import SuccessT2 from "./components/SuccessT2"
-export default function App(){
-
-  return(
-    <>
-    <div className="T1">
-    <h2>T1/Pizza + 🍕</h2>
-    <OrderForm></OrderForm>
-     <br></br>
-     <br></br>
-     <hr></hr>
-     <br></br>
-     <br></br>
-     <Home></Home>
-     <br></br>
-     <br></br>
-     <hr></hr>
-     <br></br>
-     <br></br>
-     <Success></Success>
-    </div>
-
-    <div>
-      <h1>T2/Pizza + 🍕</h1>
-      <OrderFormT2></OrderFormT2>
-      <br></br>
-     <br></br>
-     <hr></hr>
-     <br></br>
-     <br></br>
-     <HomeT2></HomeT2>
-     <br></br>
-     <br></br>
-     <hr></hr>
-     <br></br>
-     <br></br>
-     <SuccessT2></SuccessT2>
-      
-
-
-    </div>
-    </>
-   
-  )
+import OrderFormT2 from "./components/OrderFormT2";
+import HomeT2 from "./components/HomeT2";
+import SuccessT2 from "./components/SuccessT2";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/Success">
+          <SuccessT2 />
+        </Route>
+        <Route path="/Order">
+          <OrderFormT2 />
+        </Route>
+        <Route path="/">
+          <HomeT2 />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -1,20 +1,18 @@
-import { Form, FormGroup, Label, Input, ButtonGroup, Button } from 'reactstrap';
-import React from 'react';
-
+import { Form, FormGroup, Label, Input, ButtonGroup, Button } from "reactstrap";
+import React, { useSyncExternalStore } from "react";
 
 export default function Success() {
-
-    return (
-        <>
-          <div className="backgroundRed flex column padding-xl roboto ">
-            <div>
-              <img className="flex center tekno-img" src="/public/logo.svg"/>
-            </div>
-            <div className="flex column align-center roboto beyaz font-45 padding-xl">
-              <span className=''>TEBRİKLER! </span>
-              <span className=''>SİPARİŞİNİZ ALINDI!</span>
-            </div>
-          </div>
-        </>
-    );
+  return (
+    <>
+      <div className="backgroundRed flex column padding-xl roboto ">
+        <div>
+          <img className="flex center tekno-img" src="/public/logo.svg" />
+        </div>
+        <div className="flex column align-center roboto beyaz font-45 padding-xl">
+          <span className="">TEBRİKLER! </span>
+          <span className="">SİPARİŞİNİZ ALINDI!</span>
+        </div>
+      </div>
+    </>
+  );
 }
